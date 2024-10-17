7b4eef1a1698cb105223ef858b9c86c0c5151e52
Ednario Andrade <contatoednario@gmail.com>
2024-10-17 07:38:30 -0300

    refactor: Update SwaggerModule setup in main.ts

--- src/main.ts
+++ src/main.ts
@@ -9,8 +9,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const document = SwaggerModule.createDocument(app, configSwagger);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
