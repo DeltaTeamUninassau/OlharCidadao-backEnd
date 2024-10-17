217c100c9e03d6225a061e534899c9ff475f4426
Ednario Andrade <contatoednario@gmail.com>
2024-10-17 07:39:39 -0300

    refactor: Update app.listen to use a default port of 3000 if process.env.PORT is not defined

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, configSwagger);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(process.env.PORT);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
